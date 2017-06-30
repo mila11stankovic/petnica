67ac5c6d7cbf557a717527220276e916742e73f1
mila11stankovic <mila14a7@gmail.com>
2017-06-30 17:03:37 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 13:56:58 2017
+
+@author: admin
+"""
+
+
+
+rec1 = "pevač"
+rec1_sufiks = rec1 [-1]
+print(rec1_sufiks)
+rec_osnova = rec1 [0:3]
+rec_sufiks = rec1 [3:5]
